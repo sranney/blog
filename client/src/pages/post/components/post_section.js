f7304bbf8ff042c85290de60d47f5c8c5ea301d2
Spencer <spencerranney@gmail.com>
2018-03-30 02:15:42 -0500

    navigatable post toc, and cool toc hover effect

--- client/src/pages/post/components/post_section.js
+++ client/src/pages/post/components/post_section.js
@@ -2,25 +2,85 @@ import React, {Component} from "react";
 
 import Comments from "./post_comments";
 
+let clearTimeOut_ID,clearTimeOut_exit;
+
 export default class PostSection extends Component{
+    constructor(props){
+        super(props);
+        this.highlight = this.highlight.bind(this);
+        this.unhighlight = this.unhighlight.bind(this);
+    }
+    
+    highlight=function(event){
+        const el = event.target;
+        document.querySelector(".top_half").style.top=el.offsetTop+5+"px";
+        document.querySelector(".top_half").style.left=el.offsetWidth+20+"px";
+        document.querySelector(".bottom_half").style.top=el.offsetTop+5+20+"px";
+        document.querySelector(".bottom_half").style.left=el.offsetWidth+20+"px";  
+        clearTimeout(clearTimeOut_exit);
+        clearTimeOut_ID = setTimeout(()=>{
+            document.querySelector(".top_half").style.left = "-5px";
+            document.querySelector(".bottom_half").style.left = "-5px";
+            document.querySelector(".top_half").style.width = el.offsetWidth+10+"px";
+            document.querySelector(".bottom_half").style.width = el.offsetWidth+10+"px";
+        },500);
+    } 
+    
+    unhighlight = function(event){
+        clearTimeout(clearTimeOut_ID);
+        document.querySelector(".bottom_half").style.width = "20px";
+        document.querySelector(".top_half").style.width = "20px";
+        document.querySelector(".top_half").style.left=event.target.offsetWidth+20+"px";
+        document.querySelector(".bottom_half").style.left=event.target.offsetWidth+20+"px";
+        clearTimeOut_exit = setTimeout(()=>{
+            document.querySelector(".top_half").style.left = "110%";
+            document.querySelector(".bottom_half").style.left = "110%";    
+        },1000);
+    }    
+
+    goTo=function(id,listNum){
+        const offset_toc = document.querySelector(".PostTableContents").offsetTop;
+        const scrollBy_Y = document.querySelector(`#${id}`).offsetTop + 160 - (offset_toc+listNum*18);
+        window.scrollBy(0,scrollBy_Y);
+    }
+    
     render(){
+        const {post} = this.props;
+        const {sections,title,comments} = post;
+
         return (<div className="postContainer">
         <h1 className="title">
-            {this.props.post.title.split("").map((ltr,idx)=>ltr===" "?<br key={idx}/>:<div key={idx}>{ltr}</div>)}
+            {title.split("").map((ltr,idx)=>ltr===" "?<br key={idx}/>:<div key={idx}>{ltr}</div>)}
         </h1>                    
+        <ul className="PostTableContents">
+            {
+                Object.keys(sections).map((section,idx)=>{
+                    return (<li
+                                key={idx}
+                                onMouseOver = {this.highlight}
+                                onMouseLeave = {this.unhighlight}
+                                onClick={()=>this.goTo(sections[section].header.replace(/ /g,"").replace(/\?/g,"").replace(/\:/g,"").replace(/,/g,"").replace(/\!/g,"").replace(/\-/g,"").replace(/\'/g,""),idx)}
+                            >
+                                {sections[section].header}
+                            </li>)
+                })                       
+            }
+        </ul>
         {
-            Object.keys(this.props.post.sections).map((section,idx)=>{
+            Object.keys(sections).map((section,idx)=>{
                 return(
                     <div key={idx} className="sectionContainer">
-                        <p className="post_header">{this.props.post.sections[section].header}</p>
-                        <p className="post_text">{this.props.post.sections[section].text}</p>
-                        <iframe height='300' width="100%" scrolling='no' src={this.props.post.sections[section].insert} frameborder='no' allowtransparency='true' allowfullscreen='true'>
+                        <p id={`${sections[section].header.replace(/ /g,"").replace(/\?/g,"").replace(/\:/g,"").replace(/,/g,"").replace(/\!/g,"").replace(/\-/g,"").replace(/\'/g,"")}`} className="post_header">{sections[section].header}</p>
+                        <p className="post_text">{sections[section].text}</p>
+                        <iframe height='300' width="100%" scrolling='no' src={sections[section].insert} frameBorder='no' allowtransparency='true' allowFullScreen='true'>
                         </iframe>
                     </div>                       
                 )
             })                            
         }
-        <Comments comments={this.props.post.comments}/>
+        <Comments comments={comments}/>
+        <div className="top_half"></div>
+        <div className="bottom_half"></div>
     </div>)        
     }
 }
